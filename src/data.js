f870c39f59d9c4160ece901bcb877e21794f3d2f
Tatyana <taniaks2003@mail.ru>
2019-03-03 19:46:42 +0500

    Создание структуры данных. Шаблонизация верстки с использованием данных

--- src/data.js
+++ src/data.js
@@ -16,7 +16,7 @@ const ALL_FILMS = [
   `Пираты карибского моря`,
 ];
 
-const DESCRIPTION_OPTIONS = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus. `;
+const DESCRIPTION_OPTIONS = `Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras aliquet varius magna, non porta ligula feugiat eget. Fusce tristique felis at fermentum pharetra. Aliquam id orci ut lectus varius viverra. Nullam nunc ex, convallis sed finibus eget, sollicitudin eget ante. Phasellus eros mauris, condimentum sed nibh vitae, sodales efficitur ipsum. Sed blandit, eros vel aliquam faucibus, purus ex euismod diam, eu luctus nunc ante ut dui. Sed sed nisi sed augue convallis suscipit in sed felis. Aliquam erat volutpat. Nunc fermentum tortor ac porta dapibus. In rutrum ac purus sit amet tempus.`;
 
 const ALL_POSTERS = [
   `./images/posters/accused.jpg`,
@@ -26,6 +26,18 @@ const ALL_POSTERS = [
   `./images/posters/moonrise.jpg`,
   `./images/posters/three-friends.jpg`,
 ];
+
+const ALL_GENRE = [
+  `Thriller`,
+  `Comedy`,
+  `Action`,
+  `Fantastic`,
+  `Detective`,
+  `Drama`,
+  `Historical`,
+];
+
+const MAX_SENTENCES_IN_DESCRIPTION = 4;
 
 const allComments = [
   `Отлично`,
@@ -45,28 +57,27 @@ const allComments = [
 ];
 
 const proffers = DESCRIPTION_OPTIONS.split(`. `);
+
 const currentDate = new Date(Date.now());
 
 const generateRandomNumber = (maxNumber) => {
   return Math.floor(Math.random() * maxNumber);
 };
 
-const createDescription = (number) => {
-  let description = ``;
-
+const getArrayOfUniqueValues = (number, sourceArray) => {
+  const resultArray = [];
+  const tmpArray = sourceArray.slice();
   for (let i = 0; i < number; i++) {
-    description += `${proffers[generateRandomNumber(proffers.length)]}. `;
+    const index = generateRandomNumber(tmpArray.length);
+    resultArray.push(tmpArray[index]);
+    tmpArray.splice(index, 1);
   }
-  return description;
+  return resultArray;
 };
 
-const createComments = (number) => {
-  const comments = [];
-
-  for (let i = 0; i < number; i++) {
-    comments.push(allComments[generateRandomNumber(allComments.length)]);
-  }
-  return comments;
+const createDescription = () => {
+  const descriptionArray = getArrayOfUniqueValues(generateRandomNumber(MAX_SENTENCES_IN_DESCRIPTION), proffers);
+  return `${descriptionArray.join(`. `)}${descriptionArray.length ? `.` : ``}`;
 };
 
 export default () => {
@@ -75,18 +86,10 @@ export default () => {
     rating: generateRandomNumber(100) / 10,
     year: currentDate.getFullYear() - generateRandomNumber(40),
     duration: generateRandomNumber(40) + 90,
-    genre: [
-      `Thriller`,
-      `Comedy`,
-      `Action`,
-      `Fantastic`,
-      `Detective`,
-      `Drama`,
-      `Historical`,
-    ][generateRandomNumber(7)],
+    genre: ALL_GENRE[generateRandomNumber(ALL_GENRE.length)],
     poster: ALL_POSTERS[generateRandomNumber(ALL_POSTERS.length)],
-    description: createDescription(generateRandomNumber(4)),
-    comments: createComments(generateRandomNumber(14)),
+    description: createDescription(),
+    comments: getArrayOfUniqueValues(generateRandomNumber(allComments.length), allComments),
   };
   return film;
 };
